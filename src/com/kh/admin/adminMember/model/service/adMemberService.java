2e46e684018410195b78fb75da14e428ba5b3cce
LEE <LEE@DESKTOP-8GG5J6J>
2020-04-18 02:31:23 +0900

    테스트중 받지마세요

--- src/com/kh/admin/adminMember/model/service/adMemberService.java
+++ src/com/kh/admin/adminMember/model/service/adMemberService.java
@@ -1,6 +1,7 @@
 package com.kh.admin.adminMember.model.service;
 
 import java.sql.Connection;
+import java.util.ArrayList;
 
 import com.kh.admin.adminMember.model.dao.adMemberDao;
 import com.kh.admin.adminMember.model.vo.adMember;
@@ -17,6 +18,17 @@ public class adMemberService {
 		close(conn);
 		
 		return searchMember;
+	}
+
+	public ArrayList<adMember> selectList() {
+		
+		Connection conn = getConnection();
+		
+		ArrayList<adMember> list = new adMemberDao().selectList(conn);
+		close(conn);
+		return list;
+		
+		
 	}
 
 }
