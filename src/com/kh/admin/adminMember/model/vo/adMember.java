91b5f385e0c6e8d8ef4097051d26d1a642373b9a
LEE <LEE@DESKTOP-8GG5J6J>
2020-04-17 20:22:10 +0900

    servlet

--- src/com/kh/admin/adminMember/model/vo/adMember.java
+++ src/com/kh/admin/adminMember/model/vo/adMember.java
@@ -3,30 +3,62 @@ package com.kh.admin.adminMember.model.vo;
 import java.sql.Date;
 
 public class adMember {
-
-	private String memberId;      // 회원아이디
-	private String memberName;    // 회원이름
-	private String phone;         // 전화번호
-	private String email;         // 이메일
-	private Date enrollDate;      // 가입일
-	private String blacklist; 	  // 블랙리스트 여부
-	private String address;		  // 배송지 주소	
-	private String addressDetail; // 배송지 상세주소
+	
+	private String memberId;		// 회원 아이디
+	private String memberName;		// 회원 이름
+	private String gender;			// 성별
+	private String birth;			// 생년월일
+	private String email;			// 이메일
+	private String phone;			// 휴대번호
+	private Date enrollDate;		// 회원가입일
+	private String delMemberYn;		// 회원탈퇴여부
+	private Date delDate;			// 회원탈퇴일
+	private String password;		// 비밀번호
+	private Date modifyDate;		// 회원정보 수정일
+	private String blackListYn;		// 블랙리스트 여부
+	private int pointSum;			// 적립금 합계
+	private int	reportedNum;		//누적 신고횟수
+	private String refMemberId;		 //추천인아이디
 	
 	public adMember() {
 		
 	}
 
-	public adMember(String memberId, String memberName, String phone, String email, Date enrollDate, String address,
-			String addressDetail) {
+	public adMember(String memberId, String memberName, String gender, String birth, String email, String phone,
+			Date enrollDate, String delMemberYn, Date delDate, String password, Date modifyDate, String blackListYn,
+			int pointSum, int reportedNum, String refMemberId) {
 		super();
 		this.memberId = memberId;
 		this.memberName = memberName;
+		this.gender = gender;
+		this.birth = birth;
+		this.email = email;
+		this.phone = phone;
+		this.enrollDate = enrollDate;
+		this.delMemberYn = delMemberYn;
+		this.delDate = delDate;
+		this.password = password;
+		this.modifyDate = modifyDate;
+		this.blackListYn = blackListYn;
+		this.pointSum = pointSum;
+		this.reportedNum = reportedNum;
+		this.refMemberId = refMemberId;
+	}
+	
+	
+
+	/**
+	 * @author Taek
+	 * 회원가입 시 필수 입력 정보
+	 */
+	public adMember(String memberId, String password, String memberName, String phone, String email, String refMemberId) {
+		super();
+		this.memberId = memberId;
+		this.password = password;
+		this.memberName = memberName;
 		this.phone = phone;
 		this.email = email;
-		this.enrollDate = enrollDate;
-		this.address = address;
-		this.addressDetail = addressDetail;
+		this.refMemberId = refMemberId;
 	}
 
 	public String getMemberId() {
@@ -45,12 +77,20 @@ public class adMember {
 		this.memberName = memberName;
 	}
 
-	public String getPhone() {
-		return phone;
+	public String getGender() {
+		return gender;
 	}
 
-	public void setPhone(String phone) {
-		this.phone = phone;
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getBirth() {
+		return birth;
+	}
+
+	public void setBirth(String birth) {
+		this.birth = birth;
 	}
 
 	public String getEmail() {
@@ -61,6 +101,14 @@ public class adMember {
 		this.email = email;
 	}
 
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
 	public Date getEnrollDate() {
 		return enrollDate;
 	}
@@ -69,37 +117,78 @@ public class adMember {
 		this.enrollDate = enrollDate;
 	}
 
-	public String getBlacklist() {
-		return blacklist;
+	public String getDelMemberYn() {
+		return delMemberYn;
 	}
 
-	public void setBlacklist(String blacklist) {
-		this.blacklist = blacklist;
+	public void setDelMemberYn(String delMemberYn) {
+		this.delMemberYn = delMemberYn;
 	}
 
-	public String getAddress() {
-		return address;
+	public Date getDelDate() {
+		return delDate;
 	}
 
-	public void setAddress(String address) {
-		this.address = address;
+	public void setDelDate(Date delDate) {
+		this.delDate = delDate;
 	}
 
-	public String getAddressDetail() {
-		return addressDetail;
+	public String getPassword() {
+		return password;
 	}
 
-	public void setAddressDetail(String addressDetail) {
-		this.addressDetail = addressDetail;
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Date getModifyDate() {
+		return modifyDate;
+	}
+
+	public void setModifyDate(Date modifyDate) {
+		this.modifyDate = modifyDate;
+	}
+
+	public String getBlackListYn() {
+		return blackListYn;
+	}
+
+	public void setBlackListYn(String blackListYn) {
+		this.blackListYn = blackListYn;
+	}
+
+	public int getPointSum() {
+		return pointSum;
+	}
+
+	public void setPointSum(int pointSum) {
+		this.pointSum = pointSum;
+	}
+
+	public int getReportedNum() {
+		return reportedNum;
+	}
+
+	public void setReportedNum(int reportedNum) {
+		this.reportedNum = reportedNum;
+	}
+
+	public String getRefMemberId() {
+		return refMemberId;
+	}
+
+	public void setRefMemberId(String refMemberId) {
+		this.refMemberId = refMemberId;
 	}
 
 	@Override
 	public String toString() {
-		return "adMember [memberId=" + memberId + ", memberName=" + memberName + ", phone=" + phone + ", email=" + email
-				+ ", enrollDate=" + enrollDate + ", blacklist=" + blacklist + ", address=" + address
-				+ ", addressDetail=" + addressDetail + "]";
+		return "Member [memberId=" + memberId + ", memberName=" + memberName + ", gender=" + gender + ", birth=" + birth
+				+ ", email=" + email + ", phone=" + phone + ", enrollDate=" + enrollDate + ", delMemberYn="
+				+ delMemberYn + ", delDate=" + delDate + ", password=" + password + ", modifyDate=" + modifyDate
+				+ ", blackListYn=" + blackListYn + ", pointSum=" + pointSum + ", reportedNum=" + reportedNum
+				+ ", refMemberId=" + refMemberId + "]";
 	}
-	
-	
+
 	
 }
