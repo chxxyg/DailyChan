0110c1a96e38267b9293e8bd942f5d51c0720c5f
지혜 <@DESKTOP-146N3SH>
2020-04-25 00:36:44 +0900

    아이디/비밀번호 찾기 성공(아이디화면,비밀번호화면 따로 고정시킴)|로그인 실패 시 다시 redirect하게 만듦|inquiry&notice 서블릿/dao/service/vo폴더 만듦,(코드는 아직 완벽히 작성x) notice쿼리문작성|attachment vo 작성

--- src/com/kh/inquiry/model/dao/InquiryDao.java
+++ src/com/kh/inquiry/model/dao/InquiryDao.java
@@ -1,5 +1,45 @@
 package com.kh.inquiry.model.dao;
 
-public class InquiryDao {
+import static com.kh.common.JDBCTemplate.close;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import com.kh.inquiry.model.vo.Inquiry;
+
+public class InquiryDao {
+	
+	private Properties prop = new Properties();
+
+	public int insertInquiry(Connection conn, Inquiry i) {
+		
+		int result = 0;
+		PreparedStatement pstmt = null;
+		String sql = prop.getProperty("insertInquiry");
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setString(1, i.getMemberId());
+			pstmt.setString(2, i.getInquiryType());
+			pstmt.setString(3, i.getInquiryTitle());
+			pstmt.setString(4, i.getInquiryContent());
+			pstmt.setString(5, i.getInquiryFileYn());
+			
+			result = pstmt.executeUpdate();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close(pstmt);
+		}
+		return result;
+
+	}
+	
+	
+	
+	
 }
