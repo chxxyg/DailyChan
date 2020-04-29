3c7032f089bee13b42d4a973025d216777fa8700
LEE <LEE@DESKTOP-8GG5J6J>
2020-04-29 21:33:38 +0900

    관리자 - 블랙리스트 삭제 (delete)

--- src/com/kh/admin/adminBlackList/controller/AdBlackListDeleteServlet.java
+++ src/com/kh/admin/adminBlackList/controller/AdBlackListDeleteServlet.java
@@ -43,7 +43,7 @@ public class AdBlackListDeleteServlet extends HttpServlet {
 		int result2 = new adMemberService().deleteBlackList(mid);
 		
 		if(result > 0 && result2 > 0) {	// 성공
-			response.sendRedirect("blackListDetail.ad?bno=" + bno);
+			response.sendRedirect("blackList.ad");
 		}else {
 			
 		}
